b74d0659bd339998f169f4f90c0bae822db1d10e
darkun7 <182410101113@students.unej.ac.id>
2021-12-08 10:59:02 +0700

    Remove iplot

--- ml.py
+++ ml.py
@@ -34,7 +34,7 @@ from collections import Counter
 # SNA
 import networkx as nx
 from networkx.readwrite import json_graph
-from plotly.offline import download_plotlyjs, init_notebook_mode,  iplot, plot
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot
 import plotly.figure_factory as ff
 init_notebook_mode(connected=True)
 
